e53cefad30bff8f4d6e5ef0e23eb4ef6c69a79ff
Seongtaek Lim <EH71@airbears2-10-142-133-109.AirBears2.1918.Berkeley.EDU>
2015-02-10 13:28:46 -0800

    Done with q6a

--- q6a.py
+++ q6a.py
@@ -1,3 +1,4 @@
+import math
 import scipy
 import numpy as np
 import numpy.ma as ma
@@ -6,30 +7,27 @@ import sklearn
 from sklearn.cluster import KMeans
 from sklearn import metrics
 
-
-
 # Strategy for q6:
 	# build array with necessary data field (q8-q13, q16, log of q17),
 	# mask the array to clean NaN, inf, and -inf values
 	# then run Kmeans and silhouette score at k=5
 
-
 data = pd.read_table('yelp_reviewers.txt', sep='|')
 
-VA = data[['q7','q8','q9']].values
+VA = data[['q8', 'q9', 'q10', 'q11', 'q12', 'q13', 'q16a', 'q16b', 'q16c', 'q16d', 'q16e', 'q16f', 'q16g', 'q16h', 'q16i', 'q17']].values
+
+# getting natural log of q17
+for record in VA:
+  if record[-1] != 0:
+    record[-1] = math.log(record[-1])
 
 maskarr = ma.masked_array(VA, np.isnan(VA))
 maskmean = maskarr.mean()
 
 mx = ma.masked_array(VA,np.isnan(VA),fill_value=maskmean)
 
-clusters = [2,3,4,5,6,7,8]
-
-s_scores = []
-
-for i in clusters:
-	KM = sklearn.cluster.KMeans(n_clusters = clusters[i-2]).fit(mx.filled())
-	s_scores.append(sklearn.metrics.silhouette_score(mx.filled(), KM.labels_, metric='euclidean', sample_size=1000))
-	print "The Silhouette value for", i, "centroids is:", s_scores[i-2]
-
-
+k = 5
+s_score = 0
+KM = sklearn.cluster.KMeans(n_clusters = k).fit(mx.filled())
+s_score = sklearn.metrics.silhouette_score(mx.filled(), KM.labels_, metric='euclidean', sample_size=1000)
+print "The Silhouette value for", k, "centroids is:", s_score
